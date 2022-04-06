7b30d130604dc61fa4ba593c78fac96866bf17bf
rkrstacic <ray.back99@gmail.com>
2022-04-06 15:42:38 +0200

    Improved get_all_places_under

--- JsTest/Test getAllPlacesUnder/all_places_under.js
+++ JsTest/Test getAllPlacesUnder/all_places_under.js
@@ -1,48 +1,27 @@
 const places = require("./all_places");
 const allPlaces = places.get_all_places();
 
-/*
-// Old solution
-module.exports = (place_id) => {
-	let getChildById = (id) => allPlaces.filter((p) => p.parentID == id);
-	let queue = getChildById(place_id);
-	let result = [];
-
-	while (queue.length > 0) {
-		let current = queue.shift();
-		result.push(current);
-		queue.push(...getChildById(current.id));
-	}
-
-	return result;
-};
-*/
-
-// Tree implementation for Places
+// Tree class
 class TreeNode {
-	constructor(value) {
-		this.value = value;
-		this.descendants = allPlaces
-			.filter((place) => place.parentID == value.id)
-			.map((place) => new TreeNode(place));
-	}
-
-	// Helper print function
-	print(space = 0) {
-		console.log("  ".repeat(space), "L", this.value);
-		this.descendants.forEach((place) => place.print(space + 1));
+	constructor() {
+		this.value = {};
+		this.descendants = [];
 	}
 }
 
-// Tree init
-let place_root = new TreeNode({
-	id: 0,
-	name: "root",
-	parentID: -1,
-});
+function construct_place_tree(all_places, id = 0) {
+	return all_places
+		.filter((place) => place.parentID === id)
+		.map((place) => {
+			let tree_node = new TreeNode();
+			tree_node.value = place;
+			tree_node.descendants = construct_place_tree(all_places, place.id);
+			return tree_node;
+		});
+}
 
 // Helper node search function
-function node_search(search_id) {
+function node_search(place_root, search_id) {
 	let stack = [];
 	stack.push(place_root);
 
@@ -63,9 +42,14 @@ function node_search(search_id) {
 	return -1;
 }
 
+// Tree init
+let place_root = new TreeNode();
+place_root.value = { id: 0, name: "root", parentID: -1 };
+place_root.descendants = construct_place_tree(allPlaces, place_root.value.id);
+
 // Main function
 function get_all_places_under(place_id) {
-	let node = node_search(place_id);
+	let node = node_search(place_root, place_id);
 	let queue = [node];
 	let result = [];
 
@@ -78,6 +62,4 @@ function get_all_places_under(place_id) {
 	return result.map((tree_node) => tree_node.value);
 }
 
-module.exports = (place_id) => {
-	return get_all_places_under(place_id);
-};
+module.exports = (place_id) => get_all_places_under(place_id);
