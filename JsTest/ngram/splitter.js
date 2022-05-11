b7fa0f3be662c4be9654e80064ac57a94eb944c7
Rafael Krstačić <35665918+rkrstacic@users.noreply.github.com>
2022-05-11 12:44:02 +0200

    Added splitter v2

--- JsTest/ngram/splitter.js
+++ JsTest/ngram/splitter.js
@@ -1,4 +1,4 @@
-function splitter(text) {
+function splitter_v1(text) {
     const breakchars = [".", "!", "?"];
 
     // Consider triple dot as a dot
@@ -17,4 +17,69 @@ function splitter(text) {
     return text.replaceAll(reg, " . ").split(" ");
 }
 
-module.exports = { splitter };
+const breakchars = [".", "!", "?", ":"];
+const nonadditive = [",", '"'];
+const breaktoken = ".";
+let splitted, currentWord;
+
+// Handler function for end of word
+function endOfWordHandler(includeBreak = false) {
+    // console.log(currentWord);
+    if (currentWord != "") {
+        splitted.push(currentWord);
+    }
+    if (includeBreak) {
+        splitted.push(breaktoken);
+    }
+    currentWord = "";
+}
+
+// Main splitter function
+function splitter_v2(text) {
+    let stopWordReached = false;
+    let stopWordChar = "";
+
+    splitted = [];
+    currentWord = "";
+
+    for (const char of text) {
+        const breakSkippable = char == " " || nonadditive.includes(char);
+
+        // Breakchar management
+        if (breakchars.includes(char)) {
+            stopWordReached = true;
+            stopWordChar = char;
+        }
+
+        // Valid breakpoint
+        else if (stopWordReached && breakSkippable) {
+            stopWordReached = false;
+            endOfWordHandler((includeBreak = char == " "));
+        }
+
+        // Invalid breakpoint
+        else if (stopWordReached && !breakSkippable) {
+            stopWordReached = false;
+            currentWord += stopWordChar + char;
+        }
+
+        // End of word
+        else if (char == " ") {
+            endOfWordHandler();
+        }
+
+        // Non additive characters
+        else if (nonadditive.includes(char)) {
+        }
+
+        // Normal character
+        else {
+            currentWord += char;
+        }
+    }
+
+    endOfWordHandler();
+    return splitted;
+}
+
+module.exports = { splitter: splitter_v2 };
